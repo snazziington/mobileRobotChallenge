34b6d8a1ff5bfaefdfe84bdbdf86b374902d9a0a
snazziington <amini4life@gmail.com>
2026-04-19 20:49:13 +0200

    added assignment pdf to folder, and planned out my work for tomorrow
    I copied the colour detection v3 code into "objectColourDetection" and annotated it/made minor changes. Tomorrow/Tuesday I'll develop my code some more while working with the robot

--- Amini Tests/objectColourDetection.py
+++ Amini Tests/objectColourDetection.py
@@ -7,31 +7,46 @@ from picamera2 import Picamera2
 import numpy as np
 import time
 
-# Range of H in the HSV color space represented by the color
+# Range of H (hue) in the HSV color space represented by the color
+    # Hue ranges from 0 to 180
 color_dict = {'red': [0, 4], 'orange': [5, 18], 'yellow': [22, 37], 'green': [42, 85], 
               'blue': [92, 110], 'purple': [115, 165], 'red_2': [165, 180]} 
 
-#Define a 5×5 convolution kernel with element values of all 1.
-kernel_5 = np.ones((5, 5), np.uint8)
+# Define a 5×5 convolution kernel (array(?)), all values are 1
+kernel_5 = np.ones((5, 5), np.uint8) # np.uint8; 8-bit integers (max value = 255)
 
-resizeFactor = 4 # camera resolution is divided by this number
-resizedDimensions = (640 * 1 / resizeFactor, 480 * 1 / resizeFactor)
+# Determines size of image (so it's easily changeable)
+resizeFactor = 4 # Camera resolution is divided by this number
+resizedDimensions = (640 / resizeFactor, 480 / resizeFactor)
 
 def color_detect(img, color_name):
 
-    # The blue range will be different under different lighting conditions and can be adjusted flexibly.  H: chroma, S: saturation v: lightness
+    # Blue range will be different under different lighting conditions + can be adjusted flexibly.
+    # H: chroma, S: saturation v: lightness
 
-    # In order to reduce the amount of calculation, the size of the picture is reduced from (640, 480) to (160, 120)
+    # In order to reduce the amount of calculation, the size of the picture is reduced from (640, 480) to (160, 120) (divided by "resizeFactor")
+    resize_img = cv2.resize(img, resizedDimensions, interpolation=cv2.INTER_LINEAR)
 
-    resize_img = cv2.resize(img, resizedDimensions, interpolation=cv2.INTER_LINEAR)  
-    hsv = cv2.cvtColor(resize_img, cv2.COLOR_BGR2HSV) # Convert from BGR to HSV
+    # Convert colour space from BGR to HSV
+    hsv = cv2.cvtColor(resize_img, cv2.COLOR_BGR2HSV) 
+
+    # Color type is equal to the Color name parameter from this function
     color_type = color_name
     
-    mask = cv2.inRange(hsv, np.array([min(color_dict[color_type]), 60, 60]), np.array([max(color_dict[color_type]), 255, 255])) # inRange()：Make the ones between lower/upper white, and the rest black
-    if color_type == 'red':
-            mask_2 = cv2.inRange(hsv, (color_dict['red_2'][0], 0, 0), (color_dict['red_2'][1], 255, 255))
-            mask = cv2.bitwise_or(mask, mask_2)
+    # Creates a mask with following specifications
+        # If a pixel's H value is between the lower and upper bound of said colour, and the S and L ranges between 60 and 255,
+        # create a mask of those pixels
+    mask = cv2.inRange(hsv,
+                       np.array([min(color_dict[color_type]), 60, 60]),   
+                       np.array([max(color_dict[color_type]), 255, 255])) 
 
+    # TODO: No idea what's going on here
+    if color_type == 'red':
+            mask_2 = cv2.inRange(hsv,
+                                 (color_dict['red_2'][0], 0, 0),
+                                 (color_dict['red_2'][1], 255, 255))
+            mask = cv2.bitwise_or(mask, mask_2)
+    
     morphologyEx_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_5, iterations=1) # Perform an open operation on the image 
 
     # Find the contour in morphologyEx_img, and the contours are arranged according to the area from small to large.
@@ -56,24 +71,32 @@ def color_detect(img, color_name):
                 w = w * resizeFactor
                 h = h * resizeFactor
                 cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)  # Draw a rectangular frame
-                cv2.putText(img, color_type, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)# Add character description
+                cv2.putText(img, color_type, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2) # Add character description
 
     return img, mask, morphologyEx_img
 
 with Picamera2() as camera:
     print("start color detect")
 
-    camera.preview_configuration.main.size = (160, 120)
+    # Initialising camera
+    camera.preview_configuration.main.size = (resizedDimensions)
     camera.preview_configuration.main.format = "RGB888"
     camera.preview_configuration.align()
     camera.configure("preview")
     camera.start()
 
-    while True:
-        img = camera.capture_array() #frame.array
-        img, img_2, img_3 =  color_detect(img, 'red')  # Color detection function
+    # TODO: Write out
+    """
+    For the first 60 seconds, look at the background and "memorise" it.
+    When something new appears, memorise the colours within a certain range (range of hsl)
+    Create a white mask of the object and render it
+    """
+
+    while True: # loops indefinitely until "return" or "break"
+        img = camera.capture_array() # frame.array
+        img, img_2, img_3 = color_detect(img, 'red') # img 1, 2 and 3 are "img", "mask", and "morphologyEx_img" respectively
         cv2.imshow("video", img)    # OpenCV image show
-        cv2.imshow("mask", img_2)    # OpenCV image show
+        cv2.imshow("mask", img_2)   # OpenCV image show
         cv2.imshow("morphologyEx_img", img_3)    # OpenCV image show
     
         k = cv2.waitKey(1) & 0xFF
@@ -81,6 +104,7 @@ with Picamera2() as camera:
         if k == 27:
             break
 
+    # Only executes these commands once the loop is broken by pressing ESC key
     print('quit ...') 
     cv2.destroyAllWindows()
     camera.close()  
