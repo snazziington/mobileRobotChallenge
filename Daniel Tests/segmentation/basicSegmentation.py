5e432742d59917464afa41329edb4cef5d652eef
drueda <drueda4567@gmail.com>
2026-04-21 17:22:12 +0200

    turned floor detect into a function

--- Daniel Tests/segmentation/basicSegmentation.py
+++ Daniel Tests/segmentation/basicSegmentation.py
@@ -1,6 +1,31 @@
 import cv2
 import numpy as np
 
+def floorMasker(frame, width, height):
+    # Creates the frame for floor detection
+    floorReg = frame[y1:y2, x1:x2]
+    floorReg = cv2.resize(floorReg, (blurWidth, blurHeight), interpolation=cv2.INTER_LINEAR)
+    blur = cv2.blur(floorReg,(blurWidth,blurHeight))
+    floorColHSV = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    floorColPix = blur[blurHeight//2, blurWidth//2]
+    floorPixHSV = floorColHSV[blurHeight//2, blurWidth//2]
+
+    # Splits the frame into its HSV components and blurs the Value component
+    h,s,v = cv2.split(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV))
+    vblur = blur = cv2.blur(v,(10,10))
+
+    # Reconstructs the frame with the blurred Value
+    blurFrame = cv2.cvtColor(cv2.merge([h,s,vblur]), cv2.COLOR_HSV2BGR)
+
+    # Sets the floor color detection bounds
+    lowerFloor = np.array([int(floorPixHSV[0])-15, int(floorPixHSV[1])-25, 30])
+    upperFloor = np.array([int(floorPixHSV[0])+15, int(floorPixHSV[1])+25, 253])
+
+    # Creates an image mask based off of the frame with blurred Value
+    mask = cv2.inRange(cv2.cvtColor(blurFrame, cv2.COLOR_BGR2HSV), lowerFloor, upperFloor)
+
+    return mask, floorColPix, floorPixHSV
+
 windowTitle = "Basic Segmentation"
 
 cv2.namedWindow(windowTitle)
@@ -29,27 +54,8 @@ while True:
     # Flips the frame to be the right orientation
     frame = cv2.flip(frame,1)
 
-    # Creates the frame for floor detection
-    floorReg = frame[y1:y2, x1:x2]
-    floorReg = cv2.resize(floorReg, (blurWidth, blurHeight), interpolation=cv2.INTER_LINEAR)
-    blur = cv2.blur(floorReg,(blurWidth,blurHeight))
-    floorColHSV = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-    floorColPix = blur[blurHeight//2, blurWidth//2]
-    floorPixHSV = floorColHSV[blurHeight//2, blurWidth//2]
-
-    # Splits the frame into its HSV components and blurs the Value component
-    h,s,v = cv2.split(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV))
-    vblur = blur = cv2.blur(v,(10,10))
-
-    # Reconstructs the frame with the blurred Value
-    blurFrame = cv2.cvtColor(cv2.merge([h,s,vblur]), cv2.COLOR_HSV2BGR)
-
-    # Sets the floor color detection bounds
-    lowerFloor = np.array([int(floorPixHSV[0])-15, int(floorPixHSV[1])-25, 30])
-    upperFloor = np.array([int(floorPixHSV[0])+15, int(floorPixHSV[1])+25, 253])
-
-    # Creates an image mask based off of the frame with blurred Value
-    mask = cv2.inRange(cv2.cvtColor(blurFrame, cv2.COLOR_BGR2HSV), lowerFloor, upperFloor)
+    # Returns the floor detection mask and the "average" floor color in BGR and HSV
+    mask, floorColPix, floorpixHSV = floorMasker(frame, width, height)
 
     # Uncomment to draw rectangle around floor detect region
     cv2.rectangle(frame, (x1,y1), (x2,y2), (int(floorColPix[0]), int(floorColPix[1]), int(floorColPix[2])),3)
